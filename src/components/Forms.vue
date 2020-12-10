d19de068db361ec49edf5c24877aa9030d3f7046
borisenko <kk>
2020-12-11 01:44:16 +0300

    forms  2/5, computed()

--- src/components/Forms.vue
+++ src/components/Forms.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="forms-wrapper">
     <h1>Корректировка ССД</h1>
+    inputForms = {{inputForms}}
     <hr>
     <h2>ID донесения - {{field.id_ves}}</h2>
     <div>
@@ -23,7 +24,6 @@
       <div @click="onFormsAvoid" class="controls__btn">Не сохранять</div>
       <div @click="onFormsSave" class="controls__btn">Сохранить</div>
     </div>
-    field = {{field}}
   </div>
 </template>
 
@@ -35,66 +35,75 @@ export default {
       required: true
     }
   },
-  data: () => ({
+  computed: {
     inputForms: {
-      "id_rank": {
-        name: 'Категория',
-        value: 'this.field.id_rank',
-        formType: 'text'
+      get() {
+        return {
+          "id_rank": {
+            name: 'Категория',
+            value: this.field.id_rank,
+            formType: 'text'
+          },
+          "id_region": {
+            name: 'Регион',
+            value: this.field.id_region,
+            formType: 'text'
+          },
+          "id_region_to": {
+            name: 'Регион следования',
+            value: this.field.id_region_to,
+            formType: 'text'
+          },
+          "id_information_source": {
+            name: 'Источник информации',
+            value: this.field.id_information_source,
+            formType: 'text'
+          },
+          "id_regime": {
+            name: 'Код запаса',
+            value: this.field.id_regime,
+            formType: 'text'
+          },
+          "permit": {
+            name: 'Разрешение',
+            value: this.field.permit,
+            formType: 'text'
+          },
+          "date": {
+            name: 'Дата',
+            value: this.field.date,
+            formType: 'date'
+          },
+          "date_arrival": {
+            name: 'Дата прибытия',
+            value: this.field.date_arrival,
+            formType: 'date'
+          },
+          "timestamp": {
+            name: 'Дата, время',
+            value: this.field.timestamp,
+            formType: 'datetime-local'
+          },
+          "datetime": {
+            name: 'Дата, время 2',
+            value: this.field.datetime,
+            formType: 'datetime-local'
+          },
+        }
       },
-      "id_region": {
-        name: 'Регион',
-        value: '',
-        formType: 'text'
-      },
-      "id_region_to": {
-        name: 'Регион следования',
-        value: '',
-        formType: 'text'
-      },
-      "id_information_source": {
-        name: 'Источник информации',
-        value: '',
-        formType: 'text'
-      },
-      "id_regime": {
-        name: 'Код запаса',
-        value: '',
-        formType: 'text'
-      },
-      "permit": {
-        name: 'Разрешение',
-        value: '',
-        formType: 'text'
-      },
-      "date": {
-        name: 'Дата',
-        value: '',
-        formType: 'date'
-      },
-      "date_arrival": {
-        name: 'Дата прибытия',
-        value: '',
-        formType: 'date'
-      },
-      "timestamp": {
-        name: 'Дата, время',
-        value: '',
-        formType: 'datetime-local'
-      },
-      "datetime": {
-        name: 'Дата, время 2',
-        value: '',
-        formType: 'datetime-local'
-      },
+      set(val) {
+        console.log('val ==', val)
+      }
     },
-    textAreaForms: {
-      note: {
-        name: 'Примечание',
-        value: ''
+    textAreaForms() {
+      return {
+        note: {
+          name: 'Примечание',
+          value: this.field.note,
+        }
       }
     }
-  }),
+  },
   methods: {
     onFormsSave() {
       console.log('inputForms ==', this.inputForms)
